f81f24cd25d1ca5308364444355722aa7535d0af
Ben Turnbull <benbull@amazon.com>
2024-04-23 16:24:04 -0400

    amplify auth

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import React, { Component } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import HomeSimple from "./components/HomeSimple";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 // function App() {
 //   return <div className="App">
 //   </div>;
 // }
+
+Amplify.configure(awsconfig);
 
 class App extends Component {
   render() {
@@ -20,4 +26,5 @@ class App extends Component {
   }
 }
 
-export default App;
+// wrap your app in the default authenticator component
+export default withAuthenticator(App);
